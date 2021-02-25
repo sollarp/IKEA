36909f9884d025809ad975d2e3fc4031f6aaeac6
sollarp <szpitor@gmail.com>
2021-02-25 17:51:04 +0000

    up

--- Excel_Fl.py
+++ Excel_Fl.py
@@ -5,10 +5,18 @@ from pandas import ExcelFile
 
 dt = pd.read_excel('ESheet.xls')
 dr = pd.DataFrame(data=dt).T ## organize data 
+dt.dropna(inplace = True)
+sub ='Test'
+start = 0
+dt["Indexes"]= dt["Item_name"].str.find(sub, start) 
+f = dt[dt['Desc'].str.contains("Waste")][['Desc']]
+print(f.T)
+
 #print(dt)
 #print(dt['Item_name'])
-print(dr)
-print(dt.head())
+#print(dr)
+#print(dt)
+#print(dt.head())
 #print(dt.shape)
 #print(dt.head(4))
 #print(dt.tail)
