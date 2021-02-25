27762439e9798058cbdd7fcdc86798b25a6d31c6
sollarp <szpitor@gmail.com>
2021-02-25 16:50:37 +0000

    first commit

---
+++ Excel_Fl.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from pandas import ExcelWriter
+from pandas import ExcelFile
+
+
+dt = pd.read_excel('ESheet.xls')
+dr = pd.DataFrame(data=dt).T ## organize data 
+#print(dt)
+#print(dt['Item_name'])
+print(dr)
+#print(dt.head())
+#print(dt.shape)
+#print(dt.head(4))
+#print(dt.tail)
