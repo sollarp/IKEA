8e5ff0ef92edebb06c3d9efe855e817b209f79ef
sollarp <szpitor@gmail.com>
2021-02-27 22:19:46 +0000

    added more function

--- web_sitesec.py
+++ web_sitesec.py
@@ -5,26 +5,36 @@ import requests
 class OffersSite():
 
     def open_url(self):
-        url_call = "https://www.ikea.com/gb/en/offers/limited-time-offers/?filters=f-online-sellable%3Atrue&page=10"
-        req = requests.get(url_call)
-        soup = BeautifulSoup(req.text, "html.parser")
-        self.find_elements(get_soup=soup)
-
-    def find_elements(self, get_soup):
+        url = "https://www.ikea.com/gb/en/offers/limited-time-offers/?filters=f-online-sellable%3Atrue&page=10"
+        req = requests.get(url)
+        self.soup = BeautifulSoup(req.text, "html.parser")
+        #self.find_elements(get_soup=soup)
+        return self.soup
+    
+    def list_elements(self):
+        self.open_url()
         n = "range-revamp-header-section__description-text"
-        items = get_soup.find_all("span", {"class": n})
+        p = "range-revamp-price__integer"
+        v = "range-revamp-product-highlight range-revamp-product-highlight__time-restricted-offer"
+        self.find_elements(n, p, v)
+
+    def find_elements(self, *args):
+        get_soup = self.soup
+        items = get_soup.find_all("span", {"class": args})
         self.search_items(get_items=items)
 
-    # soup.find('input', {'name': 'auth_key'}).get('value')
     def search_items(self, get_items):
         items_all=[]
 
         for i in get_items:    
 
-            items_all.append(i.text)
+            self.res =items_all.append(i.text)
 
         print(items_all)
+    def valid_until(self):
+
+    
 
 run = OffersSite()
-run.open_url()
+run.list_elements()
 
