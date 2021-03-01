c9ff94b1e2a49f62026555e3ff794a86dd312ffa
sollarp <szpitor@gmail.com>
2021-03-01 22:28:23 +0000

    completed loop

--- Excel_Fl.py
+++ Excel_Fl.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pandas import ExcelWriter
 from pandas import ExcelFile
+from web_sitesec import OffersSite
 
 
 dt = pd.read_excel('ESheet.xls')
@@ -13,6 +14,7 @@ dt["Indexes"]= dt["Item_name"].str.find(sub, start)
 f = dt[dt['Desc'].str.contains("Waste")][['Desc']]
 lis = f.values.tolist() # convert dataobject to list
 print(lis[0][0])
+
 #print(dt)
 #print(dt['Item_name'])
 #print(dr)
